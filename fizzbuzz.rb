e8ae77033c12cd36787b48f9c9d7a13dabed5673
Rudia Kim <rudiakim@usc.edu>
2018-01-20 04:00:39 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,20 @@
+def fizzbuzz(int)
+  if int % 3 == 0
+   "Fizz"
+ end
+end
+
+def fizzbuzz(int)
+ if int % 5 == 0
+   "Buzz"
+end
+end
+
+
+
+
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+fizzbuzz(4)
