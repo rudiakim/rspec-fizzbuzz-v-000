5976a490636d5669afd834b5f903d916a6399159
Rudia Kim <rudiakim@usc.edu>
2018-01-20 04:55:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 0 && int % 5 !=0
    "Fizz"
 
 elsif int % 5 == 0
   "Buzz"
 
-else int % 5 && int % 3 == 0
+elsif int % 5 && int % 3 == 0
   "FizzBuzz"
 
 
